49593885fcccbf05772eb2a2f2ada249d4d2c18e
Joseph Ellis <josephellis95@gmail.com>
2024-01-16 20:08:19 -0600

    tests pass

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,36 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(true)
-})
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import AppFunctional from './AppFunctional'; 
+
+test('renders form with email input and submit button', () => {
+  const { getByPlaceholderText, getByText } = render(<AppFunctional />);
+  const emailInput = getByPlaceholderText('type email');
+  const submitButton = getByText('Submit');
+
+  expect(emailInput).toBeInTheDocument();
+  expect(submitButton).toBeInTheDocument();
+});
+
+test('typing on the input updates its value', () => {
+  const { getByPlaceholderText } = render(<AppFunctional />);
+  const emailInput = getByPlaceholderText('type email');
+
+  fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+
+  expect(emailInput.value).toBe('test@example.com');
+});
+
+test('updates email input value on change', () => {
+  const { getByPlaceholderText } = render(<AppFunctional />);
+  const emailInput = getByPlaceholderText('type email');
+
+  fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+
+  expect(emailInput.value).toBe('test@example.com');
+});
+
+
+
+
+
